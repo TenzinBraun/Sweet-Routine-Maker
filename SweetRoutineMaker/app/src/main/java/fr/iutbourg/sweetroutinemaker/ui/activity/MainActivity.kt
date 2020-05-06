6ce9ab8dbd5cbc2734a88ebb07f9becaeae72715
Martin Braun <tenzin.braun@gmail.com>
2020-05-06 15:25:38 +0200

    Initial Commit with Base Project

---
+++ SweetRoutineMaker/app/src/main/java/fr/iutbourg/sweetroutinemaker/ui/activity/MainActivity.kt
@@ -0,0 +1,13 @@
+package fr.iutbourg.sweetroutinemaker.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import fr.iutbourg.sweetroutinemaker.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
